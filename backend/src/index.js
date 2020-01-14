e5da4b54b20e5619a7a9dca415820a7d8a6e22a5
Guilherme <guilherme.ferreira@basis.com.br>
2020-01-14 17:18:08 -0300

    [UPDATE] criando estrutura base do projeto

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 const dotenv = require('dotenv');
 
 const app = express();
@@ -12,14 +13,6 @@ mongoose.connect('mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO
 });
 
 app.use(express.json());
-// Métodos HTTP: GET, POST, PUT, DELETE
-
-// Tipos de parâmetros:
-// Query Params: request.query (Filtros, ordenação, paginação
-// Route Params: request.params (Identificar um recurso na alteração ou remoção)
-// Body: request.body (Dados para criação ou alteração de um registro)
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello Omnistack 10'});
-});
+app.use(routes);
 
 app.listen(3333);
