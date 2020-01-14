63ccbfa704a2abac336e890a2c226a356b6d4e00
Guilherme <guilherme.ferreira@basis.com.br>
2020-01-14 17:52:47 -0300

    [UPDATE] criando andpoint para cadastrar um novo usuario, e buscando os dados dele no github a partir do github_username

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-
+const axios = require('axios');
 const routes = Router();
 
 // Métodos HTTP: GET, POST, PUT, DELETE
@@ -9,8 +9,12 @@ const routes = Router();
 // Route Params: request.params (Identificar um recurso na alteração ou remoção)
 // Body: request.body (Dados para criação ou alteração de um registro)
 
-routes.post('/users', (request, response) => {
+routes.post('/devs', async (request, response) => {
     console.log(request.body);
+    const { name, avatar_url, bio, github_username, techs} = request.body;
+
+    const apiresponse = await axios.get(`https://api.github.com/users/${github_username}` );
+    console.log(apiresponse.data);
     return response.json({ message: 'Hello OmniStack' });
 });
 
