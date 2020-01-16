057bebbaea598efeb24f709e02b152569b14d7aa
Guilherme <guilherme.ferreira@basis.com.br>
2020-01-16 14:10:35 -0300

    [UPDATE] aprimorando layout da aplicação

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
 
@@ -29,11 +30,48 @@ function App() {
                         </div>
                     </div>
                     <button type="submit">Salvar</button>
-
-
                 </form>
             </aside>
-            <main></main>
+            <main>
+                <ul>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars0.githubusercontent.com/u/5393392?s=460&v=4" alt="Guilherme Ferreira" />
+                            <div className="user-info">
+                                <strong>Guilherme Ferreira</strong>
+                                <span>PHP, Python</span>
+                            </div>
+
+                        </header>
+                        <p>Fullstack Developer</p>
+                        <a href="https://github.com/guilhermebferreira">Acessar perfil no Github</a>
+                    </li>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars0.githubusercontent.com/u/5393392?s=460&v=4" alt="Guilherme Ferreira" />
+                            <div className="user-info">
+                                <strong>Guilherme Ferreira</strong>
+                                <span>PHP, Python</span>
+                            </div>
+
+                        </header>
+                        <p>Fullstack Developer</p>
+                        <a href="https://github.com/guilhermebferreira">Acessar perfil no Github</a>
+                    </li>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars0.githubusercontent.com/u/5393392?s=460&v=4" alt="Guilherme Ferreira" />
+                            <div className="user-info">
+                                <strong>Guilherme Ferreira</strong>
+                                <span>PHP, Python</span>
+                            </div>
+
+                        </header>
+                        <p>Fullstack Developer</p>
+                        <a href="https://github.com/guilhermebferreira">Acessar perfil no Github</a>
+                    </li>
+                </ul>
+            </main>
         </div>
 
     );
