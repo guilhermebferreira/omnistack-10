e723949393c252d55e1122bfc1306d8ac67bfa98
Guilherme <guilherme.ferreira@basis.com.br>
2020-01-16 12:42:22 -0300

    [ADD] entendendo os conceitos básicos de react (Componente e Propriedade)

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from "./Header";
+// Componente: um bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: são informações que um componente PAI passa para o componente FILHO
+// Estado:
 
 function App() {
   return (
-      <h1>Hello World!</h1>
+      <>
+        <Header title="Titulo"/>
+        <Header title="Dashboard"/>
+      </>
   );
 }
 
